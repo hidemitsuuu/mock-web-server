a074fea74cbfbd3499c0c41a1fa2a55879a0d615
hidemitsuuu <hide19930917.git@gmail.com>
2022-09-25 01:54:49 +0900

    テストコード拡充

--- src/test/kotlin/com/example/routes/AbstractRouteTest.kt
+++ src/test/kotlin/com/example/routes/AbstractRouteTest.kt
@@ -14,15 +14,63 @@ abstract class AbstractRouteTest(
 ) {
 
     @Test
-    fun test() = testApplication {
+    fun getTest() = testApplication {
         client.get(baseUri + subUri).apply {
             assertEquals(testStatus, status)
-            assertEquals(testBody(testStatus), bodyAsText())
+            assertEquals(testBody(HttpMethod.Get), bodyAsText())
         }
     }
 
-    private fun testBody(status: HttpStatusCode): String {
-        return "{\"uri\":\"${baseUri + subUri}\",\"statusCode\":${testStatus.value},\"method\":\"GET\",\"message\":\"Request succeeded.\"}"
+    @Test
+    fun postTest() = testApplication {
+        client.post(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Post), bodyAsText())
+        }
+    }
+
+    @Test
+    fun putTest() = testApplication {
+        client.put(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Put), bodyAsText())
+        }
+    }
+
+    @Test
+    fun patchTest() = testApplication {
+        client.patch(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Patch), bodyAsText())
+        }
+    }
+
+    @Test
+    fun deleteTest() = testApplication {
+        client.delete(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Delete), bodyAsText())
+        }
+    }
+
+    @Test
+    fun headTest() = testApplication {
+        client.head(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Head), bodyAsText())
+        }
+    }
+
+    @Test
+    fun optionsTest() = testApplication {
+        client.options(baseUri + subUri).apply {
+            assertEquals(testStatus, status)
+            assertEquals(testBody(HttpMethod.Options), bodyAsText())
+        }
+    }
+
+    private fun testBody(method: HttpMethod): String {
+        return "{\"uri\":\"${baseUri + subUri}\",\"statusCode\":${testStatus.value},\"method\":\"${method.value}\",\"message\":\"Request succeeded.\"}"
     }
 
 }
